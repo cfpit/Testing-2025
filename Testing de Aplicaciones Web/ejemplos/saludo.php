d3a7e09ed7aadff669f6955620ce757569cbbc74
cfpit <127329179+cfpit@users.noreply.github.com>
2025-04-25 18:35:42 -0300

    Add files via upload

---
+++ Testing de Aplicaciones Web/ejemplos/saludo.php
@@ -0,0 +1,5 @@
+<?php
+    echo "Hola Mundo desde PHP!";
+    echo "<br>";// salto de linea
+    echo "<h1 style='color:red'>Hola Argentina</h1>";//h1: titulo
+?>
---
+++ Testing de Aplicaciones Web/ejemplos/servidor.php
@@ -0,0 +1,6 @@
+<?php
+// captura de datos del formulario
+$nombre = $_POST['nombre'];
+
+//muestro el saludo
+echo "Hola $nombre !!";
